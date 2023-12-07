dc4597d56f6ec7cace40ffc5034707d6f1efcc9b
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-12-07 20:33:15 +0100

    Optimize case where src_dir == dst_dir

--- src/reduce.py
+++ src/reduce.py
@@ -8,9 +8,7 @@ from PIL import Image
 QUALITY = 75
 
 
-def process_images(src_dir: Path, dst_dir: Path, recursive: bool, resize: bool, quality: int = QUALITY) -> None:
-    print(f"JPEG quality = {quality}\n", flush=True)
-
+def different_paths(src_dir: Path, dst_dir: Path, recursive: bool, resize: bool, quality: int) -> None:
     src_paths = src_dir.glob("*") if not recursive else src_dir.glob("**/*")
 
     for src_path in src_paths:
@@ -23,14 +21,40 @@ def process_images(src_dir: Path, dst_dir: Path, recursive: bool, resize: bool, 
                     image = image.resize(new_size, Image.Resampling.LANCZOS)
 
                 dst_path: Path = dst_dir / src_path.relative_to(src_dir)
-                print(f"Resized \"{src_path}\" to \"{dst_path}\".", flush=True)
                 dst_path.parent.mkdir(parents=True, exist_ok=True)
 
                 # The argument `optimize` is used for both JPEGs and PNGs.
                 # The argument `quality` is used for JPEGs, and simply ignored in case of PNGs.
                 image.save(dst_path, optimize=True, quality=quality)
+                print(f"Resized \"{src_path}\" to \"{dst_path}\".", flush=True)
         except Exception as e:
             print(e)
+
+
+def same_paths(src_dir: Path, recursive: bool, resize: bool, quality: int) -> None:
+    src_paths = src_dir.glob("*") if not recursive else src_dir.glob("**/*")
+
+    for src_path in src_paths:
+        try:
+            with Image.open(src_path) as image:
+                if resize:
+                    image_size = image.size
+                    new_size = (image_size[0] // 2, image_size[1] // 2)
+                    image = image.resize(new_size, Image.Resampling.LANCZOS)
+
+                image.save(src_path, optimize=True, quality=quality)
+                print(f"Resized \"{src_path}\".", flush=True)
+        except Exception as e:
+            print(e)
+
+
+def process_images(src_dir: Path, dst_dir: Path, recursive: bool, resize: bool, quality: int) -> None:
+    print(f"JPEG quality = {quality}\n", flush=True)
+
+    if src_dir != dst_dir:
+        different_paths(src_dir, dst_dir, recursive, resize, quality)
+    else:
+        same_paths(src_dir, recursive, resize, quality)
 
 
 def main() -> None:
