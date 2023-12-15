eba0306cc6e48352a50f0c8694db1ea0cd788d8c
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2023-12-15 22:06:04 +0100

    Remove flushing stdout in loops

--- src/reduce.py
+++ src/reduce.py
@@ -26,7 +26,7 @@ def different_paths(src_dir: Path, dst_dir: Path, recursive: bool, resize: bool,
                 # The argument `optimize` is used for both JPEGs and PNGs.
                 # The argument `quality` is used for JPEGs, and simply ignored in case of PNGs.
                 image.save(dst_path, optimize=True, quality=quality)
-                print(f"Resized \"{src_path}\" to \"{dst_path}\".", flush=True)
+                print(f"Resized \"{src_path}\" to \"{dst_path}\".")
         except Exception as e:
             print(e)
 
@@ -43,7 +43,7 @@ def same_paths(src_dir: Path, recursive: bool, resize: bool, quality: int) -> No
                     image = image.resize(new_size, Image.Resampling.LANCZOS)
 
                 image.save(src_path, optimize=True, quality=quality)
-                print(f"Resized \"{src_path}\".", flush=True)
+                print(f"Resized \"{src_path}\".")
         except Exception as e:
             print(e)
 
